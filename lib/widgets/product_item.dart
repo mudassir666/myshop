81b65d8a1af49629d7b7f45b5d7585f0087d2148
mudassir666 <mudassir666666666@gmail.com>
2021-09-05 23:39:31 +0500

    video:4 working on the products grid and item widgets

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  //const ProductItem({ Key? key }) : super(key: key);
+
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem({
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        leading: IconButton(onPressed: (){}, icon: Icon(Icons.favorite),),
+        backgroundColor: Colors.black54,
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(onPressed: (){}, icon: Icon(Icons.shopping_cart),),
+      ),
+    );
+  }
+}
