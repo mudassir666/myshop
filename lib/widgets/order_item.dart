a9fa62c43a5ad93b608811af8006c8a1854df23c
mudassir666 <mudassir666666666@gmail.com>
2021-09-17 02:27:11 +0500

    video 27 : Making orders Expandable & statefull widget vs provider

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,13 +1,22 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:myshop/models/orders.dart' as ord;
 
 //simpaly change the class name as OrderItemWidget
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   //when u change the class name you dont need to use ord.
   final ord.OrderItem order;
 
   OrderItem({required this.order});
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -16,15 +25,48 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('\$${order.amount}'),
+            title: Text('\$${widget.order.amount}'),
             subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime),
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
             ),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
-          )
+          ),
+          if (_expanded)
+            Container(
+              // if the value a is smaller it will take that otherwise it will take value b
+              height: min(widget.order.products.length * 20.0 + 10, 100),
+              padding: EdgeInsets.symmetric(horizontal: 15,vertical: 4),
+              child: ListView(
+                children: widget.order.products
+                    .map((prod) => Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              prod.title,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              '${prod.quantity}x \$${prod.price}',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.grey,
+                              ),
+                            )
+                          ],
+                        ))
+                    .toList(),
+              ),
+            )
         ],
       ),
     );
