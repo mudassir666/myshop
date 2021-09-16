ac94a3cdc2bc9b4016a9e2b8f492003ae55b6293
mudassir666 <mudassir666666666@gmail.com>
2021-09-17 01:25:16 +0500

    video 25 :Adding an order screen

---
+++ lib/screens/orders_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:myshop/models/orders.dart' show Orders;
+import 'package:myshop/widgets/order_item.dart';
+import 'package:provider/provider.dart';
+
+class OrdersScreen extends StatelessWidget {
+  //const OrdersScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final orderData = Provider.of<Orders>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Orders'),
+      ),
+      body: ListView.builder(
+        itemCount: orderData.orders.length,
+        itemBuilder: (ctx, i) => OrderItem(order: orderData.orders[i]),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/order_item.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:myshop/models/orders.dart' as ord;
+
+//simpaly change the class name as OrderItemWidget
+class OrderItem extends StatelessWidget {
+  //when u change the class name you dont need to use ord.
+  final ord.OrderItem order;
+
+  OrderItem({required this.order});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('\$${order.amount}'),
+            subtitle: Text(
+              DateFormat('dd MM yyyy hh:mm').format(order.dateTime),
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.expand_more),
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
