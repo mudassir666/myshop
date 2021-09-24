9b94249d6abcf2086c9e67753c6bc5268b758eb0
mudassir666 <mudassir666666666@gmail.com>
2021-09-24 05:05:43 +0500

    video 9 : image input & image preview

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -10,13 +10,35 @@ class _EditProductScreenState extends State<EditProductScreen> {
 // to focus on a single input
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imgUrlController = TextEditingController();
+  //step 1 : creating custom listner
+  final _imgUrlFocusNode = FocusNode();
+
+//step 5 : when the update function rebuild state then it run initstate after listning
+  @override
+  void initState() {
+    _imgUrlFocusNode.addListener(_updateImageUrl);
+    super.initState();
+  }
 
 // to clear the memory when we leave the screen
   @override
   void dispose() {
+    // step 6 : remove the imgUrlFocusNode listner
+    _imgUrlFocusNode.removeListener(_updateImageUrl);
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imgUrlController.dispose();
+    //step 2 : distoring when exist page
+    _imgUrlFocusNode.dispose();
     super.dispose();
+  }
+
+ //step 4 : check if imgUrlnode has focus or not , when the imgUrlnode loses focus it will update it by setState
+  void _updateImageUrl() {
+    if (!_imgUrlFocusNode.hasFocus) {
+      setState(() {});
+    }
   }
 
   @override
@@ -60,6 +82,43 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 keyboardType: TextInputType.multiline,
                 focusNode: _descriptionFocusNode,
               ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: EdgeInsets.only(
+                      top: 8,
+                      right: 10,
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      width: 1,
+                      color: Colors.grey,
+                    )),
+                    child: _imgUrlController.text.isEmpty
+                        ? Text('Enter a URL')
+                        : FittedBox(
+                            child: Image.network(
+                              _imgUrlController.text,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Image URL'),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      // using controller to show the image before submitting
+                      controller: _imgUrlController,
+                      // step 3 : to focus on this form
+                      focusNode: _imgUrlFocusNode,
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
