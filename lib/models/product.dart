3fa63ccaa61c6f49f81317a70204b73e04b01a85
mudassir666 <mudassir666666666@gmail.com>
2021-10-21 18:13:21 +0500

    video 19 : Updating the favorite status optimistically

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,10 +20,36 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  // to void code duplication
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    // saving the old favorite status
+    final oldStatus = isFavorite;
     // if it is true it will return false and if it is false it will return true
     isFavorite = !isFavorite;
-    
     notifyListeners();
+    final url =
+        'https://flutter-update-c572d-default-rtdb.firebaseio.com/products/$id.json';
+
+    try {
+      final response = await http.patch(
+        Uri.parse(url),
+        body: json.encode({
+          'isFavorite': isFavorite,
+        }),
+      );
+      // if the response returning statuscode higher than 400 undo it
+      if (response.statusCode >= 400) {
+        // to undo the action
+        _setFavValue(oldStatus);
+      }
+    } // if error occur undo the action 
+    catch (error) {
+      _setFavValue(oldStatus);
+    }
   }
 }
