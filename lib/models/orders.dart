209e19f179b5ff25fbe30d3dc4983268787a9ec8
mudassir666 <mudassir666666666@gmail.com>
2021-10-23 16:20:08 +0500

    video 20 : Storing Orders in the Web

--- lib/models/orders.dart
+++ lib/models/orders.dart
@@ -1,5 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 import 'cart.dart';
 
 // blue print for Orders
@@ -24,17 +25,44 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
-    // it will add the product at the begging of the list
-    _orders.insert(
-      0,
-      OrderItem(
-        id: DateTime.now().toString(),
-        amount: total,
-        products: cartProducts,
-        dateTime: DateTime.now(),
-      ),
-    );
-    notifyListeners();
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    const url =
+        'https://flutter-update-c572d-default-rtdb.firebaseio.com/orders.json';
+    // to store the same time in server and locally
+    final timestamp = DateTime.now();
+
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: json.encode({
+          'amount': total,
+          'dateTime': timestamp.toIso8601String(),
+          // want to map each products in to the list
+          'products': cartProducts
+              .map((cp) => {
+                    'id': cp.id,
+                    'title': cp.title,
+                    'quantity': cp.quantity,
+                    'price': cp.price,
+                  })
+              .toList(),
+        }),
+      );
+      // it will add the product at the begging of the list
+      _orders.insert(
+        0,
+        OrderItem(
+          id: json.decode(response.body)['name'],
+          amount: total,
+          products: cartProducts,
+          dateTime: timestamp,
+        ),
+      );
+      notifyListeners();
+    } 
+    catch (error) {
+      throw error;
+    }
   }
+
 }
--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -33,33 +33,13 @@ class CartScreen extends StatelessWidget {
                     label: Text(
                       '\$${cart.totalAmount.toStringAsFixed(2)}',
                       style: TextStyle(
-                        color: Theme.of(context).primaryTextTheme.headline6!.color,
+                        color:
+                            Theme.of(context).primaryTextTheme.headline6!.color,
                       ),
                     ),
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      //converting cart items.object into a list of cart items.object instead of passing whole map
-                      Provider.of<Orders>(context, listen: false).addOrder(
-                        cart.items.values.toList(),
-                        cart.totalAmount,
-                      );
-                      // after passing items to order we want to clear the cart
-                      cart.clear();
-                    },
-                    child: Text(
-                      'ORDER NOW',
-                      style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                      ),
-                    ),
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                        Theme.of(context).primaryTextTheme.headline6!.color,
-                      ),
-                    ),
-                  ),
+                  OrderButton(cart: cart),
                 ],
               ),
             ),
@@ -80,6 +60,63 @@ class CartScreen extends StatelessWidget {
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+// extract the orderButton to rebuild the less and make it stateful to show loading spinner
+
+class OrderButton extends StatefulWidget {
+  const OrderButton({
+    Key? key,
+    required this.cart,
+  }) : super(key: key);
+
+  final Cart cart;
+
+  @override
+  State<OrderButton> createState() => _OrderButtonState();
+}
+
+class _OrderButtonState extends State<OrderButton> {
+  // to show loading spinner
+  var _isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      // if the amount smaller then 0 and showing loading spinner button will be disable
+      onPressed: (widget.cart.totalAmount <= 0 || _isLoading)
+          ? null
+          : () async {
+              // it will show the loading spinner while adding the order in database
+              setState(() {
+                _isLoading = true;
+              });
+              //converting cart items.object into a list of cart items.object instead of passing whole map
+              await Provider.of<Orders>(context, listen: false).addOrder(
+                widget.cart.items.values.toList(),
+                widget.cart.totalAmount,
+              );
+              // it will stop showing loading spinner just after the order has been added to database
+              setState(() {
+                _isLoading = false;
+              });
+              // after passing items to order we want to clear the cart
+              widget.cart.clear();
+            },
+      child: _isLoading
+          ? CircularProgressIndicator()
+          : Text(
+              'ORDER NOW',
+              style: TextStyle(
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(
+          Theme.of(context).primaryTextTheme.headline6!.color,
+        ),
       ),
     );
   }
